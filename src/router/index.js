f26ceb4e2d94f6598cd200312189d89f51ca6a52
llx123 <573381010@qq.com>
2018-10-30 22:43:17 +0800

    layout

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,25 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+import Layout from '@/pages/layout/index'
+
 export const routes = [
   {
     path: '/login',
     component: () => import('@/pages/login/index')
-  },
+  }, {
+    path: '',
+    component: Layout,
+    redirect: 'dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/pages/dashboard/index'),
+        name: 'Dashboard',
+        meta: { title: 'dashboard', icon: 'dashboard' }
+      }
+    ]
+  }
 ]
 
 export default new Router({
