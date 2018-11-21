3b47b1d8bfa34fee658f20da599e48f7a8f0e3f5
llx123 <573381010@qq.com>
2018-11-21 12:17:54 +0800

    sideBar

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,32 @@ export default {
 }
 </script>
 
-<style lang="postcss" scoped>
+<style lang="postcss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  & .elLayout {
+    & .el-scrollbar {
+      & .scrollbar-wrapper {
+        overflow-x: hidden !important;
+        & .el-scrollbar__view {
+          height: 100%;
+        }
+      }
+    }
+  }
+  & .openBar {
+    & .main-content{
+      margin-left: 192px;
+    }
+  }
+  & .closeBar {
+    & .main-content{
+      margin-left: 56px;
+    }
+  }
 }
 </style>
--- src/pages/layout/index.vue
+++ src/pages/layout/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <side-bar />
-    <div class="main-container">
+  <div :class="{openBar: !isOpen, closeBar: isOpen, elLayout: true}">
+    <side-bar @showBar="showBar"/>
+    <div class="main-content">
       <navbar />
       <content-main />
     </div>
@@ -13,17 +13,43 @@ import { Navbar, ContentMain, SideBar } from './components'
 export default {
   data() {
     return {
-
+      isOpen: true
     }
   },
   components: {
     Navbar,
     ContentMain,
     SideBar,
+  },
+  methods: {
+    showBar(b) {
+      this.isOpen = b
+    }
   }
 }
 </script>
 
 <style lang="postcss" scoped>
-
+.elLayout {
+  & .el-scrollbar {
+    bottom: 0;
+    font-size: 0;
+    height: 100%;
+    left: 0;
+    overflow: hidden;
+    position: fixed;
+    top: 0;
+    transition: width .28s;
+  }
+}
+.openBar {
+  & .el-scrollbar {
+    width: 200px;
+  }  
+}
+.closeBar {
+  & .el-scrollbar {
+    width: 64px;
+  }  
+}
 </style>
