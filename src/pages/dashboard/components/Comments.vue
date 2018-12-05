78c0982c13253fb914dc7e2af9ded8eb262eaf47
llx123 <573381010@qq.com>
2018-12-05 17:50:08 +0800

    comments

---
+++ src/pages/dashboard/components/Comments.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="comments">
+    <el-scrollbar class="card-scroll">
+      <p>comments</p>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="postcss" scoped>
+.comments {
+  height: 200px;
+  background-color: #fff;
+}
+</style>
