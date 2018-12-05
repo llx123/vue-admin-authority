65248dffe41c6cd043fbd1221dd9d07807231c0d
llx123 <573381010@qq.com>
2018-12-05 17:50:18 +0800

    rencentSales

---
+++ src/pages/dashboard/components/RencentSales.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="rencent-sales">
+    <el-scrollbar class="card-scroll">
+      <el-table
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄',
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+.rencent-sales {
+  height: 200px;
+  background-color: #fff;
+}
+</style>
