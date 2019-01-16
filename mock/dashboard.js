e41903cf23d65453431926da5342484b495f318e
llx123 <573381010@qq.com>
2019-01-16 20:51:09 +0800

    dashboard

---
+++ mock/dashboard.js
@@ -0,0 +1,40 @@
+import Mock from 'mockjs'
+
+const Dashboard = Mock.mock({
+  'numbers': [
+    {
+      'icon': 'el-icon-view',
+      'title': 'Online Review',
+      'number|1000-3000': 1000,
+      'color': '#64ea91'
+    },
+    {
+      'icon': 'el-icon-star-on',
+      'title': 'New Customers',
+      'number|2000-4000': 2000,
+      'color': '#8fc9fb'
+    },
+    {
+      'icon': 'el-icon-tickets',
+      'title': 'Active Projects',
+      'number|3000-5000': 3000,
+      'color': '#d897eb'
+    },
+    {
+      'icon': 'el-icon-goods',
+      'title': 'Referrals',
+      'number|3000-6000': 3000,
+      'color': '#f69899'
+    },
+  ],
+  'sales|7': [
+    {
+      'name|+1': 2010,
+      'Clothes|200-500': 1,
+      'Food|180-400': 1,
+      'Electronics|300-550': 1,
+    },
+  ],
+})
+
+export default Dashboard
--- src/services/dashboard.js
+++ src/services/dashboard.js
@@ -1,8 +1,8 @@
 import request from '@/utils/request'
 
-
-
-
-
-
-
+export function getDashboardList() {
+  return request({
+    url: '/dashboard/list',
+    method: 'get',
+  })
+}
