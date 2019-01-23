7e6a8aa405e2b792aee58f37d7f31033ca9aa0e5
llx123 <573381010@qq.com>
2019-01-23 16:00:41 +0800

    sideBar

--- src/components/SidebarItem.vue
+++ src/components/SidebarItem.vue
@@ -2,35 +2,25 @@
   <div v-if="item.show || item.children">
     <!-- 带子菜单 -->
     <template v-if="item.children">
-      <!-- 需要展示子菜单 -->
-      <el-submenu v-for="(p,i) in item.children" :key="i" :index="path" v-if="p.show && !item.meta.single">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>{{p.meta ? p.meta.title : ''}}</span>
-        </template>
-        <!-- <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item :index="path">选项1</el-menu-item>
-          <el-menu-item :index="path">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item :index="path">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu :index="path">
-          <template slot="title">选项4</template>
-          <el-menu-item :index="path">选项1</el-menu-item>
-        </el-submenu> -->
-      </el-submenu>
-      <!-- 不展示子菜单 -->
-      <el-menu-item v-for="(p,i) in item.children" :key="i"
-        :index="path===''?p.path:path"
-        v-if="p.show && item.meta.single"
-        @click="doRouter(path===''?p.path:path)">
-        <i class="el-icon-menu"></i>
-        <span slot="title" class="menu-link">
-          {{p.meta?p.meta.title:''}}
-        </span>
-      </el-menu-item>
+      <template v-for="(p,i) in item.children" >
+        <!-- 需要展示子菜单 -->
+        <el-submenu :key="i" :index="path" v-if="p.show && !item.meta.single">
+          <template slot="title">
+            <i :class="p.meta?p.meta.icon:''"></i>
+            <span>{{p.meta ? p.meta.title : ''}}</span>
+          </template>
+        </el-submenu>
+        <!-- 不展示子菜单 -->
+        <el-menu-item :key="i"
+          :index="path===''?p.path:path"
+          v-if="p.show && item.meta.single"
+          @click="doRouter(path===''?p.path:path)">
+          <i :class="p.meta?p.meta.icon:''"></i>
+          <span slot="title" class="menu-link">
+            {{p.meta?p.meta.title:''}}
+          </span>
+        </el-menu-item>
+      </template>
     </template>
     <!-- 不带子菜单 -->
     <template v-else>
