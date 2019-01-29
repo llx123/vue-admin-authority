8270c624b08f9593b876dc49e4bfc40d43573d61
llx123 <573381010@qq.com>
2019-01-29 18:17:19 +0800

    posts

---
+++ src/pages/posts/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    111
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
