8990e861d25c079113b176045bd51b61fb974981
llx123 <573381010@qq.com>
2019-04-05 13:57:15 +0800

    slider

--- src/pages/ui/components/Animate.vue
+++ src/pages/ui/components/Animate.vue
@@ -9,7 +9,7 @@
         </li>
       </ul>
     </div>
-    <div class="bottomView">
+    <div class="bottomView" ref="bottomContent">
       <ol class="clear" ref="oOl">
         <li v-for="(item,index) in imgList" :key="index">
           <img :src="item.src" alt>
@@ -30,7 +30,10 @@ export default {
   data() {
     return {
       iNow: 1,
-      topWdith: 600,
+      topWidth: 600,
+      bottomWidth: 588,
+      canBtnClick: true,
+      canImgClick: true,
       len: 6,
       imgList: [
         { src: img1 },
@@ -50,15 +53,16 @@ export default {
   },
   watch: {
     iNow(val, oldVal) {
-      console.log(val, oldVal);
       let oUl = this.$refs.oUl;
       let oOl = this.$refs.oOl;
-      let topWdith = this.topWdith;
+      let topWidth = this.topWidth;
+      let boWidth = oOl.children[0].offsetWidth * 1.125;
 
       if (val > oldVal && val - oldVal === 1) {
+        this.animate(oOl, { left: -oldVal * boWidth });
         this.animate(
           oUl,
-          { left: -val * topWdith },
+          { left: -val * topWidth },
           {
             complete: () => {
               if (val === this.len + 1) {
@@ -68,11 +72,18 @@ export default {
           }
         );
       } else if (Math.abs(oldVal - val) === this.len) {
-        oUl.style.left = -topWdith * val + "px";
+        oUl.style.left = -topWidth * val + "px";
+        oOl.style.left = -boWidth * (val - 1) + "px";
       } else if (val < oldVal && val - oldVal === -1) {
+        if (val === 0) {
+          oOl.style.left = -boWidth * this.len + "px";
+          this.animate(oOl, { left: -boWidth * (this.len - 1) });
+        } else {
+          this.animate(oOl, { left: -boWidth * (val - 1) });
+        }
         this.animate(
           oUl,
-          { left: -val * topWdith },
+          { left: -val * topWidth },
           {
             complete: () => {
               if (val === 0) {
@@ -82,6 +93,15 @@ export default {
           }
         );
       }
+      for (let i = 0; i < oOl.children.length; i++) {
+        if (i !== val) {
+          oOl.children[i].style.border = "none";
+        }
+      }
+      if (val === 0) {
+        oOl.children[this.len].style.border = "2px solid red";
+      }
+      oOl.children[val].style.border = "2px solid red";
     }
   },
   methods: {
@@ -91,23 +111,71 @@ export default {
       let oOl = this.$refs.oOl;
       let aLi2 = oOl.children;
       let iNow = this.iNow;
-      let topWdith = this.$refs.topContent.offsetWidth;
-      this.topWdith = topWdith;
-      if (aLi1.length === this.imgList.length) {
+      let topWidth = this.$refs.topContent.offsetWidth;
+      let bottomWidth = this.$refs.bottomContent.offsetWidth;
+
+      this.topWidth = topWidth;
+      this.bottomWidth = bottomWidth;
+
+      if (aLi1.length === this.len) {
         let oLi1 = aLi1[0].cloneNode(true);
         let oLi2 = aLi1[aLi1.length - 1].cloneNode(true);
         oUl.appendChild(oLi1);
         oUl.insertBefore(oLi2, aLi1[0]);
+
+        let oLi3 = aLi2[0].cloneNode(true);
+        let oLi4 = aLi2[1].cloneNode(true);
+        let oLi5 = aLi2[aLi2.length - 1].cloneNode(true);
+
+        oOl.appendChild(oLi3);
+        oOl.appendChild(oLi4);
+        oOl.insertBefore(oLi5, aLi2[0]);
+
+        for (let i = 0; i < aLi2.length; i++) {
+          aLi2[i].nowIndex = i;
+        }
       }
 
-      oUl.style.width = topWdith * aLi1.length + "px";
-      oUl.style.left = -topWdith * iNow + "px";
+      oUl.style.width = topWidth * aLi1.length + "px";
+      oUl.style.left = -topWidth * iNow + "px";
+
+      oOl.style.width = (bottomWidth * aLi2.length) / 3 + "px";
+      oOl.style.left = -(iNow - 1) * oOl.children[0].offsetWidth * 1.125 + "px";
+
+      oOl.children[iNow].style.border = "2px solid red";
+      oOl.addEventListener("click", e => {
+        if (
+          e.target &&
+          e.target.parentNode.nodeName.toUpperCase() === "LI" &&
+          this.canImgClick &&
+          this.canBtnClick
+        ) {
+          if (this.iNow === e.target.parentNode.nowIndex) {
+            return false;
+          } else {
+            this.iNow = e.target.parentNode.nowIndex;
+            this.canImgClick = false;
+            setTimeout(() => {
+              this.canImgClick = true;
+            }, 550);
+          }
+        }
+      });
     },
     btnClick(num) {
-      if (this.iNow > this.len || this.iNow <= 0) {
+      if (
+        this.iNow > this.len ||
+        this.iNow <= 0 ||
+        !this.canBtnClick ||
+        !this.canImgClick
+      ) {
         return false;
       }
       this.iNow += num;
+      this.canBtnClick = false;
+      setTimeout(() => {
+        this.canBtnClick = true;
+      }, 550);
     },
     /*
      * 参数说明:
@@ -166,6 +234,9 @@ export default {
     getStyle(obj, sName) {
       return (obj.currentStyle || getComputedStyle(obj, false))[sName];
     }
+  },
+  destroyed() {
+    this.$refs.oOl.removeEventListener("click");
   }
 };
 </script>
@@ -182,16 +253,17 @@ export default {
     height: 60px;
     line-height: 60px;
     position: absolute;
+    bottom: 13%;
     background: #ccc;
     z-index: 666;
     cursor: pointer;
     text-decoration: none;
   }
   & .pre {
-    left: 0;
+    left: -2%;
   }
   & .next {
-    right: 0;
+    right: -0.3%;
   }
   & .topView,
   & .bottomView {
@@ -212,6 +284,7 @@ export default {
 .topView,
 .bottomView {
   margin: 0 auto;
+  overflow: hidden;
   & ul,
   & ol {
     margin: 0;
@@ -234,5 +307,16 @@ export default {
 }
 .bottomView {
   width: 88%;
+  & ol {
+    & li {
+      width: 10%;
+      margin-right: 1.25%;
+      box-sizing: border-box;
+      cursor: pointer;
+    }
+    & li:last-child {
+      margin-right: 0;
+    }
+  }
 }
 </style>
